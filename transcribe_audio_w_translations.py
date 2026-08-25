8d64f5801169cb699da5354ea8017661932adeba
Philip Leclerc <philip.leclerc@gmail.com>
2026-08-25 19:21:22 +0900

    Basic bugs fixed, checkpointing reliability improved, initial small test of audio transcription done

--- transcribe_audio_w_translations.py
+++ transcribe_audio_w_translations.py
@@ -1,11 +1,11 @@
 # Standard libraries
-import glob, json, os
+import glob, json, os, pathlib
 
 # Non-standard libraries
 import fire
 from transformers import pipeline
 
-def transcribeSingleAudioFile(transcriber, audio_file_path, language="japanese")
+def transcribeSingleAudioFile(transcriber, audio_file_path, language="japanese"):
     result = transcriber(
         audio_file_path,
         generate_kwargs={"language": language},
@@ -13,13 +13,12 @@ def transcribeSingleAudioFile(transcriber, audio_file_path, language="japanese")
     )
     return result
 
-def transcribeAudioFiles(audio_fpaths_input_glob_expr, output_jsonl_fpath, output_mode="a"):
+def transcribeAudioFiles(audio_fpaths_input_glob_expr, output_jsonl_fpath, output_mode="a", test_len=None):
     print(f"Received audio input fpaths glob expr: {audio_fpaths_input_glob_expr}")
     print("Initializing transcriber as Hugging Faces pipeline...")
     transcriber = pipeline(
         task="automatic-speech-recognition", model="openai/whisper-large-v3"
     )
-
 
     # NOTE our initial application involves running this on a highly unstable old laptop, so it is critical to checkpoint/restart from checkpoints
     data = []
@@ -27,13 +26,20 @@ def transcribeAudioFiles(audio_fpaths_input_glob_expr, output_jsonl_fpath, outpu
         with open(output_jsonl_fpath, encoding="utf-8") as rf:
             data = [json.loads(line) for line in rf.readlines()]
 
-    with open(output_jsonl_fpath, encoding='utf8', mode=putput_mode) as wf:
-        for index, audio_fp in enumerate(glob.glob(audio_fpaths_input_glob_expr)):
+    audio_fpaths = glob.glob(audio_fpaths_input_glob_expr)
+    audio_fpaths = audio_fpaths[:test_len] if test_len is not None else audio_fpaths
+
+    with open(output_jsonl_fpath, encoding='utf8', mode=output_mode) as wf:
+        for index, audio_fp in enumerate(audio_fpaths):
             if index >= len(data) - 1:
-                print(f"Beginning transcription of: {audio_fp}")
+                print(f"Beginning transcription for audio file # {index+1} of {len(audio_fpaths)}: {audio_fp}")
                 result = transcribeSingleAudioFile(transcriber, audio_fp, language="japanese")
-                wf.write(json.dumps(result, ensure_ascii=False) + '\n') 
-                print(f"Dumped Whisper transcription to {output_jsonl_fpath}: {result}")
+                print(f"\tWhisper returned: {result}")
+                result["source_file"] = pathlib.Path(audio_fp).stem + pathlib.Path(audio_fp).stem
+                wf.write(json.dumps(result, ensure_ascii=False) + '\n')
+                wf.flush()
+                os.fsync(wf.fileno()) # flush + fsync to try to be especially certain we write to checkpointing file before moving on
+                print(f"\tDumped Whisper transcription to {output_jsonl_fpath}: {result}")
 
 def generateLLMTranslations():
     raise NotImplementedError()
